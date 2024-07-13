f85daff6dc4c68c5c232f645bbf0890fd20a017f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-13 20:27:37 +0700

    fix: change icon and size

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MoonIcon, SunIcon } from 'lucide-react'
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
 
@@ -23,8 +23,8 @@ export function Header() {
             className="mr-6"
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
           >
-            <SunIcon className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle Theme</span>
           </Button>
         </nav>
