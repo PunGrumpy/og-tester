435c9049d5e7cd33adabaf5a711a65496c57ce1a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-14 23:54:38 +0700

    chore: remove print

--- lib/cors.ts
+++ lib/cors.ts
@@ -5,9 +5,6 @@ const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || 'http://localhost:3000'
 export async function cors(request: Request, response: NextResponse) {
   const origin = request.headers.get('origin') || ''
 
-  console.log('Request Origin:', origin)
-  console.log('Allowed Origin:', ALLOWED_ORIGIN)
-
   if (!origin || origin === ALLOWED_ORIGIN) {
     response.headers.set('Access-Control-Allow-Origin', origin || '*')
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
@@ -16,7 +13,6 @@ export async function cors(request: Request, response: NextResponse) {
       'Content-Type, Authorization, x-api-key'
     )
   } else {
-    console.log('CORS check failed')
     return new NextResponse(null, { status: 403 })
   }
 
