dafc01be62c822b55cb5400aa7f743b0a273985f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-04 18:56:57 +0700

    fix(provider): simplify component structure by removing unused icons and Toaster

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,32 +1,10 @@
 'use client'
 
 import {
-  CircleAlertIcon,
-  CircleCheckIcon,
-  CircleXIcon,
-  InfoIcon
-} from 'lucide-react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
-import * as React from 'react'
-
-import { Toaster } from './ui/sonner'
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps
+} from 'next-themes'
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <>
-      <NextThemesProvider {...props}>
-        {children}
-        <Toaster
-          position="top-center"
-          icons={{
-            success: <CircleCheckIcon size={18} />,
-            error: <CircleXIcon size={18} />,
-            warning: <CircleAlertIcon size={18} />,
-            info: <InfoIcon size={18} />
-          }}
-        />
-      </NextThemesProvider>
-    </>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
