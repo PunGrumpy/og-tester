9439779d5e381463c40ed52d90bdb76eafc8a017
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-05 12:20:46 +0700

    refactor(*): remove error, not-found, and loading

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="container mx-auto flex h-screen items-center justify-center">
-      <div className="size-8 animate-spin rounded-full border-4 border-t-transparent"></div>
-      <p className="ml-4 text-lg font-semibold">Loading...</p>
-    </div>
-  )
-}
