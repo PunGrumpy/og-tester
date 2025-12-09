78dd560c5be5e658e8497a7538a4d79e18254c5d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-09 21:56:27 +0700

    feat(animation): introduce ViewAnimation component for enhanced motion effects in view transitions

---
+++ components/view-animation.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { motion, useReducedMotion } from 'motion/react'
+import type { ReactNode } from 'react'
+
+type ViewAnimationProps = {
+  initial?: Record<string, string | number>
+  whileInView?: Record<string, string | number>
+  animate?: Record<string, string | number>
+  delay?: number
+  // className?: ComponentProps<typeof motion.div>['className'];
+  className?: string
+  children: ReactNode
+}
+
+export const ViewAnimation = ({
+  initial,
+  whileInView,
+  animate,
+  delay,
+  className,
+  children
+}: ViewAnimationProps) => {
+  const shouldReduceMotion = useReducedMotion()
+
+  if (shouldReduceMotion) {
+    return children
+  }
+
+  return (
+    <motion.div
+      animate={animate}
+      className={className}
+      initial={{ filter: 'blur(4px)', ...initial }}
+      transition={{ delay, duration: 0.8 }}
+      viewport={{ once: true, amount: 0.5 }}
+      whileInView={{ filter: 'blur(0px)', ...whileInView }}
+    >
+      {children}
+    </motion.div>
+  )
+}
