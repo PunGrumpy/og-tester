ee37ba64585ed2b24b56f14134ac80b893a30c86
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-10 15:24:24 +0700

    refactor(section): restructure Section component to improve layout with nested div for better styling control

--- components/section.tsx
+++ components/section.tsx
@@ -4,7 +4,9 @@ import { cn } from '@/lib/utils'
 type SectionProps = HTMLAttributes<HTMLDivElement>
 
 export const Section = ({ children, className, ...props }: SectionProps) => (
-  <section {...props} className="relative mx-auto max-w-6xl">
-    <div className={cn('md:border-x', className)}>{children}</div>
+  <section {...props}>
+    <div className="relative mx-auto max-w-6xl">
+      <div className={cn('md:border-x', className)}>{children}</div>
+    </div>
   </section>
 )
