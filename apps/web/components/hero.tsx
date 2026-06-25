5c71ddb443327fc0b8d35b10df901ca3c670d967
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-06-25 21:45:12 +0700

    style(web): make hero layout responsive and scale text size on mobile

--- apps/web/components/hero.tsx
+++ apps/web/components/hero.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export const Hero = () => (
   <Section
     className={cn(
-      "aspect-2/1 select-none p-4 sm:aspect-3/1",
+      "select-none p-6 py-12 sm:py-0 sm:aspect-3/1",
       "flex items-center justify-center",
       "pattern-background bg-foreground/2"
     )}
@@ -16,7 +16,7 @@ export const Hero = () => (
         initial={{ opacity: 0, translateY: -8 }}
         whileInView={{ opacity: 1, translateY: 0 }}
       >
-        <h1 className="text-balance text-center font-semibold text-5xl md:text-7xl">
+        <h1 className="text-balance text-center font-semibold text-4xl xs:text-5xl md:text-7xl">
           Open Graph Tester
         </h1>
       </ViewAnimation>
@@ -25,7 +25,7 @@ export const Hero = () => (
         initial={{ opacity: 0, translateY: -8 }}
         whileInView={{ opacity: 1, translateY: 0 }}
       >
-        <p className="max-w-2xl text-pretty text-center text-lg text-muted-foreground">
+        <p className="max-w-2xl text-pretty text-center text-base sm:text-lg text-muted-foreground">
           Test and preview your Open Graph and Twitter Card metadata. See how
           your links will appear when shared on social media platforms.
         </p>
